10496ba0d75235c53eb6a3a6a168cfa64150abf8
Aadrij Upadya <adrij2005@gmail.com>
2022-06-28 23:43:39 -0700

    Initial Commit

---
+++ Flash Chat iOS13/Controllers/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Aadrij Upadya on 6/18/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Model/message.swift
@@ -0,0 +1,13 @@
+//
+//  message.swift
+//  Flash Chat iOS13
+//
+//  Created by Aadrij Upadya on 6/18/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+}
